c1ec6d810ba5d27dcea68a07e4e7a5fcf0fa319a
xyzFahim <xyzfahim99@gmail.com>
2024-01-09 04:24:23 +0600

    DONE

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,4 @@
+// age checker script 
 function openAgeChecker() {
     document.getElementById("ageCheckerModal").style.display = "flex";
 }
@@ -7,19 +8,33 @@ function closeAgeChecker() {
 }
 
 function checkAge() {
-    var age = document.getElementById("age").value;
-    age = parseInt(age);
-    
-        if (age >= 18) {
-            document.getElementById("result").innerHTML = "<div class='resultcard'>You are an adult</div>";
-        } else if (age >= 0) {
-            document.getElementById("result").innerHTML = "<div class='resultcard'>You are a child</div>";
-        } else {
-            document.getElementById("result").innerHTML = "<div class='resultcard'>Invalid Age!</div>";
-        }
+    var ageInput = document.getElementById("age");
+    var age = ageInput.value.trim();
+
+    var resultContainer = document.getElementById("result");
+
+    if (age === "") {
+        resultContainer.innerHTML = "<div class='resultcard invalid-age-card'>Enter your age</div>";
+        return;
     }
 
+    age = parseInt(age);
 
+    if (age >= 18) {
+
+        resultContainer.innerHTML = "<div class='resultcard adult-card'><img src='Assets/img/Adult1.jpg' alt='Adult Image'><p>You are an adult</p></div>";
+
+    } else if (age >= 0) {
+        resultContainer.innerHTML = "<div class='resultcard child-card'><img src='Assets/img/Child1.jpg' alt='Child Image'><p>You are a child</p></div>";
+    } else {
+
+        resultContainer.innerHTML = "<div class='resultcard invalid-age-card'>Invalid Age!</div>";
+    }
+}
+
+
+
+// window scrolling animation 
     window.addEventListener("scroll", function(){
         var header = document.querySelector("nav");
         header.classList.toggle("sticky", window.scrollY > 0);
